92f3f7585e8dcfeb90d16189cbfcf24d54e9d77f
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-05-22 12:10:37 -0500

    create home

--- src/pages/nav-bar/home.tsx
+++ src/pages/nav-bar/home.tsx
@@ -1,13 +1,14 @@
 import React, {FC} from 'react';
 import {Link} from "react-router-dom";
-import './home.module.scss';
+import styles from './home.module.scss';
 
 
 export const Home: FC = () => {
     return (
         <nav>
             <div>
-                <Link to="/"><img className={'navbar-logo'}  src="https://cdn.appdesign.dev/wp-content/uploads/2015/09/logo-icon.png" alt="Logo" /></Link>
+                <img className={styles.logo}
+                src="https://www.shutterstock.com/image-vector/vector-illustration-cow-pig-goat-600nw-1377523625.jpg" alt="Logo" />
             </div>
             <ul>
                 <li><Link to="/">Home</Link></li>
@@ -15,9 +16,7 @@ export const Home: FC = () => {
                 <li><Link to="/contact">Contact</Link></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
-            <div>
-                <Link to="/login"><button>Login</button></Link>
-            </div>
+
         </nav>
     );
 }
