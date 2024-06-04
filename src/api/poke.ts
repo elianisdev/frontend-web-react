e5222ed2712f435b6cc2cf425c6a3875e37f42f4
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-06-03 20:09:24 -0500

    Api information into card

--- src/api/poke.ts
+++ src/api/poke.ts
@@ -1,11 +1,10 @@
 import {instance} from "./base.api";
 
-const endpoint = 'v2/pokemon/150';
 
 export const poke = {
     getAll: function ({id}: {id: number}){
-        return instance.get(endpoint, {params: {id}});
-        }
+        return instance.get(`v2/pokemon/${id}`);
+    }
 }
 
 
