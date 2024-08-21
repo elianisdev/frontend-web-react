63001b1dc21fb1e69af94bc9e0d36a93b5b94828
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-08-21 08:41:43 -0500

    form

--- src/pages/store/form-information.tsx
+++ src/pages/store/form-information.tsx
@@ -29,8 +29,7 @@ export const FormInformation = () => {
                                    placeholder="Ingrese primer apellido"/>
                         </div>
                         <div className="px-3 mb-6">
-                            <label htmlFor="secondLastName" className="block mb-2 text-sm font-medium text-gray-900">Segundo
-                                Apellido*</label>
+
                             <input type="text" id="secondLastName"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                    placeholder="Ingrese segundo apellido"/>
