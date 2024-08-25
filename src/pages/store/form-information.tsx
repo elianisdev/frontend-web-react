179a26148b142a8e00a64a1de1c874afd2422afd
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-08-25 09:29:35 -0500

    Hook Use form

--- src/pages/store/form-information.tsx
+++ src/pages/store/form-information.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import  {useForm} from 'react-hook-form';
 
 export const FormInformation = () => {
+    const { register } = useForm()
+
     return (
         <div className='contain-content py-3 px-10'>
             <h2 className="w-full text-3xl text-white my-4 float-right rounded-lg bg-gradient-to-r from-green-500 via-blue-800 to-blue-300 border-r-4 py-2 px-4">Información</h2>
@@ -10,22 +13,29 @@ export const FormInformation = () => {
                         <div className="px-3 mb-6">
                             <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900">
                                 Nombres*</label>
-                            <input type="text" id="Names"
-                                   className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                   placeholder="Ingrese sus nombres"/>
+                            <input
+                                type="text"
+                                { ... register('nombre') }
+                                id="Names"
+                               className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                              placeholder="Ingrese sus nombres"/>
                         </div>
 
                         <div className="px-3 mb-6">
                             <label htmlFor="lastName" className="block mb-2 text-sm font-medium text-gray-900">Primer
                                 Apellido*</label>
-                            <input type="text" id="lastName"
+                            <input type="text"
+                                   { ... register('apellido') }
+                                   id="lastName"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                    placeholder="Ingrese primer apellido"/>
                         </div>
                         <div className="px-3 mb-6">
                             <label htmlFor="secondLastName" className="block mb-2 text-sm font-medium text-gray-900">Segundo
                                 Apellido*</label>
-                            <input type="text" id="secondLastName"
+                            <input type="text"
+                                   { ... register('segundo apellido') }
+                                   id="secondLastName"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                    placeholder="Ingrese segundo apellido"/>
                         </div>
@@ -34,20 +44,26 @@ export const FormInformation = () => {
                         <div className="px-3 mb-6">
                         <label htmlFor="birthDate" className="block mb-2 text-sm font-medium text-gray-900">Fecha de
                                 Nacimiento*</label>
-                            <input type="date" id="birthDate"
+                            <input type="date"
+                                   { ... register('fechaNacimiento') }
+                                   id="birthDate"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"/>
                         </div>
                         <div className="px-3 mb-6">
                             <label htmlFor="gender"
                                    className="block mb-2 text-sm font-medium text-gray-900">Género*</label>
-                            <input type="text" id="gender"
+                            <input type="text"
+                                   { ... register('genero') }
+                                   id="gender"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                    placeholder="Ingrese género"/>
                         </div>
                         <div className="px-3 mb-6">
                             <label htmlFor="civilStatus" className="block mb-2 text-sm font-medium text-gray-900">Estado
                                 Civil*</label>
-                            <input type="text" id="civilStatus"
+                            <input type="text"
+                                   { ... register('estado civil') }
+                                   id="civilStatus"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
                                    placeholder="Ingrese estado civil"/>
                         </div>
