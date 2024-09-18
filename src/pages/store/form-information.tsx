3c245db598f7f4b527530e45a1a77b84f145dbc7
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-09-18 15:25:41 -0500

    Hook Use submit data

--- src/pages/store/form-information.tsx
+++ src/pages/store/form-information.tsx
@@ -39,7 +39,7 @@ export const FormInformation = () => {
                                    {...register('segundo apellido')}
                                    id="secondLastName"
                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
-                                   placeholder="Ingrese segundo apellido"/>
+                                   placeholder="Ingrese segundo apellido."/>
                         </div>
                     </div>
                     <div className="w-full md:w-1/2">
