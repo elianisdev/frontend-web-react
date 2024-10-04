e086b7f566b43ef3eab83b8d1cb49df3c146144a
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-04 16:34:28 -0500

    modal

--- src/pages/store/modal2.tsx
+++ src/pages/store/modal2.tsx
@@ -36,6 +36,18 @@ export const Modal2 = ({isOpen, onClose}: Modal2Props) => {
                                 </label>
                             </div>
                             <div>
+                                <label htmlFor="" className="mr-3 font-semibold font-[Poppins]">
+                                    <input type="text"
+                                           className="w-64 px4 border-2 border-gray-300 rounded-lg focus:outline- none focus:border-indigo-500 duration-200"/>Edad
+                                </label>
+                            </div>
+                            <div>
+                                <label htmlFor="" className="mr-3 font-semibold font-[Poppins]">
+                                    <input type="text"
+                                           className="w-64 px4 border-2 border-gray-300 rounded-lg focus:outline- none focus:border-indigo-500 duration-200"/>Correo
+                                </label>
+                            </div>
+                            <div>
                                 <button className="bg-red-400 py-2 px-6 rounded-sm text-cyan-50 font-bold m-5"
                                         onClick={() => setOpenModal(false)}>
                                     Close Modal
