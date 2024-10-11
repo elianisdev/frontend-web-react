5b34a8564176393263889c94a5a6b9c76c92ce4a
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-11 10:49:40 -0500

    form

---
+++ src/pages/Form/form.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const MyForm = () => {
+    return (
+        <div>
+<h1 className= 'w-full text-3xl text-white my-4 float-right rounded-lg bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 border-r-4 py-2 px-4'> formulario</h1>
+        </div>
+    );
+};
+
