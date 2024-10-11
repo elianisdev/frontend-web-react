3fb8fa7dfffd373948cef21995f0364f0f22f87d
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-11 11:34:20 -0500

    form

--- src/pages/Form/form.tsx
+++ src/pages/Form/form.tsx
@@ -60,6 +60,56 @@ export const MyForm = () => {
                             />
                             {errors.apellido2 && <p className="text-red-500 text-xs">Este campo es requerido</p>}
                         </div>
+                        <div>
+                            <label htmlFor="email"
+                                   className="block mb-2 text-sm font-medium text-gray-900">Correo*</label>
+                            <input
+                                type="email"
+                                {...register('email', {required: true})}
+                                id="email"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                placeholder="Ingrese su correo"
+                            />
+                            {errors.email && <p className="text-red-500 text-xs">Este campo es requerido</p>}
+                        </div>
+                        <div>
+                            <label htmlFor="phone"
+                                   className="block mb-2 text-sm font-medium text-gray-900">Teléfono*</label>
+                            <input
+                                type="tel"
+                                {...register('phone', {required: true})}
+                                id="phone"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                                placeholder="Ingrese su teléfono"
+                            />
+                            {errors.phone && <p className="text-red-500 text-xs">Este campo es requerido</p>}
+                        </div>
+                        <div>
+                            <label htmlFor="birthDate"
+                                   className="block mb-2 text-sm font-medium text-gray-900">Fecha de Nacimiento*</label>
+                            <input
+                                type="date"
+                                {...register('fechaNacimiento', {
+                                    required: {value: true, message: "Fecha de nacimiento es requerida, por favor ingrese su fecha de nacimiento"},
+                                    validate: (value) => {
+                                        const date = new Date(value);
+                                        const currentDate = new Date();
+                                        const edad = currentDate.getFullYear() - date.getFullYear();
+                                        return edad >= 18 || "Debe ser mayor de edad, por favor ingrese una fecha válida";
+                                    }
+                                })}
+                                id="birthDate"
+                                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+                            />
+                        </div>
+                        <div className="flex justify-end space-x-2 mt-5">
+                            <button className="bg-blue-600 text-white py-2 px-6 rounded-lg mt-4 flex items-center justify-center">
+                                Enviar
+                            </button>
+                            <button className="bg-red-400 text-white px-5 py-2 rounded-lg mt-4 flex items-center justify-center">
+                                Cancelar
+                            </button>
+                        </div>
 
                     </div>
                 </form>
