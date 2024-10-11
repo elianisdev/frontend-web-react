b451b28f180e5fc21991172a8ff6a4caec1c2f3b
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-11 11:11:31 -0500

    form

--- src/pages/Form/form.tsx
+++ src/pages/Form/form.tsx
@@ -3,8 +3,13 @@ import React from 'react';
 export const MyForm = () => {
     return (
         <div>
-<h1 className= 'w-full text-3xl text-white my-4 float-right rounded-lg bg-gradient-to-r from-blue-500 via-blue-800 to-blue-300 border-r-4 py-2 px-4'> formulario</h1>
-        </div>
+<h1 className= 'w-full text-3xl text-white my-4 float-right rounded-lg bg-gradient-to-r from-blue-500 via-blue-800 to-pink-500 border-r-4 py-2 px-4'> formulario</h1>
+            <div className='flexcontain-layout px-4 mx-auto xl:container'>
+                <form className="flex gap-4 p-4">
+
+                </form>
+            </div>
+      </div>
     );
 };
 
