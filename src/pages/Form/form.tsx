b2bede7952c703118ca22c7a2a7561ee605002e9
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-16 11:39:25 -0500

    openModal

--- src/pages/Form/form.tsx
+++ src/pages/Form/form.tsx
@@ -6,6 +6,10 @@ export const MyForm = () => {
 
     const [ isOpen, setIsOpen ] = useState(true);
 
+    const openModal = () => {
+        setIsOpen(false);
+
+    }
 
     const onSubmit = handleSubmit((data) => {
         console.log(data);
@@ -104,7 +108,7 @@ export const MyForm = () => {
                                     <button data-modal-target="popup-modal" data-modal-toggle="popup-modal"
                                             className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                                             type="button"
-                                            onClick={() => setIsOpen(false)}>
+                                            onClick={ openModal }>
                                         Abrir modal
                                     </button>
 
