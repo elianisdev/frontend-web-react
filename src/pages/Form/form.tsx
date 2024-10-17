ea3e7994d5908d3eecdd6dbb75b12a2c92a46272
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-17 14:06:43 -0500

    open modal and close modal

--- src/pages/Form/form.tsx
+++ src/pages/Form/form.tsx
@@ -3,20 +3,32 @@ import { useForm } from "react-hook-form";
 
 export const MyForm = () => {
     const { register, handleSubmit, formState: { errors }, setValue, reset } = useForm();
-    const [isOpen, setIsOpen] = useState(false);
 
-    const openModal = () => {
-        setIsOpen(true);
+    const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
+
+    const [isSubmitModalOpen, setIsSubmitModalOpen] = useState(false);
+
+    const openCancelModal = () => {
+        setIsCancelModalOpen(true);
     }
 
-    const closeModal = () => {
-        setIsOpen(false);
+    const closeCancelModal = () => {
+        setIsCancelModalOpen(false);
+    }
+
+    const openSubmitModal = () => {
+        setIsSubmitModalOpen(true);
+    }
+
+    const closeSubmitModal = () => {
+        setIsSubmitModalOpen(false);
     }
 
     const onSubmit = handleSubmit((data) => {
         console.log(data);
         setValue('nombre', '');
         reset();
+        closeSubmitModal();
     });
 
     return (
@@ -24,7 +36,7 @@ export const MyForm = () => {
             <div>
                 <h1 className='mb-6 w-full text-3xl text-white text-center my-4 float-right rounded-lg bg-gradient-to-r from-blue-500 via-blue-800 to-pink-500 border-r-4 py-2 px-4'>Formulario</h1>
                 <div className='flexcontain-layout px-4 mx-auto xl:container'>
-                    <form onSubmit={onSubmit} className="flex flex-col gap-4 p-4 mb-6">
+                    <form onSubmit={(e) => { e.preventDefault(); openSubmitModal(); }} className="flex flex-col gap-4 p-4 mb-6">
                         <div className="w-full md:w-1/2">
                             <div className="px-3 mb-6">
                                 <label htmlFor="firstName" className="block mb-2 text-sm font-medium text-gray-900">Nombres*</label>
@@ -97,19 +109,19 @@ export const MyForm = () => {
                             <button
                                 className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
                                 type="button"
-                                onClick={openModal}>
+                                onClick={openCancelModal}>
                                 Cancelar
                             </button>
                         </div>
                     </form>
 
-                    {isOpen && (
+                    {isCancelModalOpen && (
                         <div className="fixed inset-0 flex items-center justify-center z-50">
-                            <div className="fixed inset-0 bg-black opacity-50" onClick={closeModal}></div>
+                            <div className="fixed inset-0 bg-black opacity-50" onClick={closeCancelModal}></div>
                             <div className="bg-white rounded-lg shadow dark:bg-gray-700 p-4 md:p-5 relative">
                                 <button type="button"
                                         className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
-                                        onClick={closeModal}>
+                                        onClick={closeCancelModal}>
                                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
                                     </svg>
@@ -119,12 +131,41 @@ export const MyForm = () => {
                                     <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to cancel?</h3>
                                     <button type="button"
                                             className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
-                                            onClick={closeModal}>
+                                            onClick={closeCancelModal}>
                                         Yes, I'm sure
                                     </button>
                                     <button type="button"
                                             className="py-2.5 px-5 ml-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-                                            onClick={closeModal}>
+                                            onClick={closeCancelModal}>
+                                        Cancel
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+
+                    {isSubmitModalOpen && (
+                        <div className="fixed inset-0 flex items-center justify-center z-50">
+                            <div className="fixed inset-0 bg-black opacity-50" onClick={closeSubmitModal}></div>
+                            <div className="bg-white rounded-lg shadow dark:bg-gray-700 p-4 md:p-5 relative">
+                                <button type="button"
+                                        className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        onClick={closeSubmitModal}>
+                                    <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                                    </svg>
+                                    <span className="sr-only">Close modal</span>
+                                </button>
+                                <div className="text-center">
+                                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to submit?</h3>
+                                    <button type="button"
+                                            className="text-white bg-blue-600 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
+                                            onClick={onSubmit}>
+                                        Yes, I'm sure
+                                    </button>
+                                    <button type="button"
+                                            className="py-2.5 px-5 ml-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
+                                            onClick={closeSubmitModal}>
                                         Cancel
                                     </button>
                                 </div>
