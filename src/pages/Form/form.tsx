1879e9a759f56e67b361863e66c78574f196699d
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-18 11:25:50 -0500

    open modal and close modal

--- src/pages/Form/form.tsx
+++ src/pages/Form/form.tsx
@@ -120,7 +120,7 @@ export const MyForm = () => {
                             <div className="fixed inset-0 bg-black opacity-50" onClick={closeCancelModal}></div>
                             <div className="bg-white rounded-lg shadow dark:bg-gray-700 p-4 md:p-5 relative">
                                 <button type="button"
-                                        className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        className="absolute top-3 right-2.5 text-red-400  rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
                                         onClick={closeCancelModal}>
                                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
