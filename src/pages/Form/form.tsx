a998f63f6029c818136ca6902de145e136a409a9
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-18 12:17:54 -0500

    Language change modal

--- src/pages/Form/form.tsx
+++ src/pages/Form/form.tsx
@@ -128,16 +128,16 @@ export const MyForm = () => {
                                     <span className="sr-only">Close modal</span>
                                 </button>
                                 <div className="text-center">
-                                    <h3 className="mb-5 text-lg font-normal text-white dark:text-gray-400">Are you sure you want to cancel?</h3>
+                                    <h3 className="mb-5 text-lg font-normal text-white dark:text-gray-400">¿Estás segura de que quieres cancelar?</h3>
                                     <button type="button"
                                             className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                                             onClick={closeCancelModal}>
-                                        Yes, I'm sure
+                                        Si, estoy segura
                                     </button>
                                     <button type="button"
                                             className="py-2.5 px-5 ml-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                             onClick={closeCancelModal}>
-                                        Cancel
+                                        Cancelar
                                     </button>
                                 </div>
                             </div>
@@ -157,16 +157,16 @@ export const MyForm = () => {
                                     <span className="sr-only">Close modal</span>
                                 </button>
                                 <div className="text-center">
-                                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to submit?</h3>
+                                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">¿Estás segura de que quieres cancelar?</h3>
                                     <button type="button"
                                             className="text-white bg-blue-400 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                                             onClick={onSubmit}>
-                                        Yes, I'm sure
+                                        Si, estoy segura
                                     </button>
                                     <button type="button"
                                             className="py-2.5 px-5 ml-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
                                             onClick={closeSubmitModal}>
-                                        Cancel
+                                        Cancelar
                                     </button>
                                 </div>
                             </div>
