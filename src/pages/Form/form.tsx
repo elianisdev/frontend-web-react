82e0e225569c54b1b4b9bfdb15f91df3a9e04ac5
Eliana Suancha Guzman <eliana.guzman@techy-we.com>
2024-10-18 11:37:06 -0500

    open modal and close modal

--- src/pages/Form/form.tsx
+++ src/pages/Form/form.tsx
@@ -120,7 +120,7 @@ export const MyForm = () => {
                             <div className="fixed inset-0 bg-black opacity-50" onClick={closeCancelModal}></div>
                             <div className="bg-white rounded-lg shadow dark:bg-gray-700 p-4 md:p-5 relative">
                                 <button type="button"
-                                        className="absolute top-3 right-2.5 text-red-400  rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
+                                        className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white"
                                         onClick={closeCancelModal}>
                                     <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
                                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
@@ -128,7 +128,7 @@ export const MyForm = () => {
                                     <span className="sr-only">Close modal</span>
                                 </button>
                                 <div className="text-center">
-                                    <h3 className="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to cancel?</h3>
+                                    <h3 className="mb-5 text-lg font-normal text-gray-600 dark:text-gray-400">Are you sure you want to cancel?</h3>
                                     <button type="button"
                                             className="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center"
                                             onClick={closeCancelModal}>
